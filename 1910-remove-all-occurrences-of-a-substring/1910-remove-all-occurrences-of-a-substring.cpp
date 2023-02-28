ba9f5db12d85d5e9be51617b64e892e8335363a8
Avinash Pathak <114324142+avinashpathak06@users.noreply.github.com>
2023-02-28 10:33:10 +0530

    Time: 6 ms (40.93%), Space: 6.7 MB (66.52%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -4,6 +4,7 @@ public:
         int pos=s.find(part);
         
         while(pos!=string::npos){
+         // while(pos>0){
             s.erase(pos,part.length());
             pos=s.find(part);
         }
