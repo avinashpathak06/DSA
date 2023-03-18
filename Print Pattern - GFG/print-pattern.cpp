80714b4758b19963cb5eadac826875e60db62ef0
Avinash Pathak <114324142+avinashpathak06@users.noreply.github.com>
2023-03-18 13:01:37 +0530

    Added solution - LeetHub

---
+++ Print Pattern - GFG/print-pattern.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> pattern(int N){
+        // code here
+        
+         vector<int> v;
+
+        v.push_back(N);
+
+        int i = N - 5;
+
+        v.push_back(i);
+
+        while(i > 0){
+
+            i = i - 5;
+
+            v.push_back(i);
+
+        }
+
+        while(i != N){
+
+            i = i + 5;
+
+            v.push_back(i);
+
+        }
+
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        
+        Solution ob;
+        vector<int> ans = ob.pattern(N);
+        for(int u: ans)
+            cout<<u<<" ";
+        cout<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
