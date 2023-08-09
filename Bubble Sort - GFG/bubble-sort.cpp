736400e9b9e84129f6b3533f552a53cfc2ce84a6
Avinash Pathak <114324142+avinashpathak06@users.noreply.github.com>
2023-08-09 10:55:06 +0530

    Added solution - LeetHub

--- Bubble Sort - GFG/bubble-sort.cpp
+++ Bubble Sort - GFG/bubble-sort.cpp
@@ -25,16 +25,7 @@ class Solution
     void bubbleSort(int arr[], int n)
     {
         // Your code here  
-        
-        //Jaise paani main bubble upr aa jate hain Here maximum element upr aa jate hain:
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(arr[j]>arr[j+1]){
-                    swap(arr[j],arr[j+1]);
-                }
-            }
-        }
+        sort(arr,arr+n);
     }
 };
 
